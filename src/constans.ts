90bf71b6288bd03d1dbf9a1bc3a21dd474fccc02
Janne Valkealahti <janne.valkealahti@gmail.com>
2021-05-22 19:06:52 +0100

    Constant names

---
+++ src/constans.ts
@@ -0,0 +1,2 @@
+export const BINDER_KAFKA_NAME = 'kafka-zk';
+export const BINDER_RABBIT_NAME = 'rabbitmq';
--- test/binders.test.ts
+++ test/binders.test.ts
@@ -1,5 +1,6 @@
 import { execYtt } from '../src/ytt';
 import { findDeployment } from '../src/k8s-helper';
+import { BINDER_RABBIT_NAME, BINDER_KAFKA_NAME } from '../src/constans';
 
 describe('binders', () => {
   it('should have rabbit settings', async () => {
@@ -7,10 +8,10 @@ describe('binders', () => {
     expect(result.success).toBeTruthy();
     const yaml = result.stdout;
 
-    const rabbitDeployment = findDeployment(yaml, 'rabbitmq');
+    const rabbitDeployment = findDeployment(yaml, BINDER_RABBIT_NAME);
     expect(rabbitDeployment).toBeTruthy();
 
-    const kafkaDeployment = findDeployment(yaml, 'kafka-zk');
+    const kafkaDeployment = findDeployment(yaml, BINDER_KAFKA_NAME);
     expect(kafkaDeployment).toBeFalsy();
   });
 
@@ -19,10 +20,10 @@ describe('binders', () => {
     expect(result.success).toBeTruthy();
     const yaml = result.stdout;
 
-    const kafkaDeployment = findDeployment(yaml, 'kafka-zk');
+    const kafkaDeployment = findDeployment(yaml, BINDER_KAFKA_NAME);
     expect(kafkaDeployment).toBeTruthy();
 
-    const rabbitDeployment = findDeployment(yaml, 'rabbitmq');
+    const rabbitDeployment = findDeployment(yaml, BINDER_RABBIT_NAME);
     expect(rabbitDeployment).toBeFalsy();
   });
 });
