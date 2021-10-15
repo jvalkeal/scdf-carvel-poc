3f6e7f97403f4b70f750267976bfa066aa742ea1
Janne Valkealahti <janne.valkealahti@gmail.com>
2021-10-15 15:57:03 +0100

    For testing break lint

--- src/constants.ts
+++ src/constants.ts
@@ -10,7 +10,7 @@ export const GRAFANA_NAME = 'grafana';
 export const PROMETHEUS_NAME = 'prometheus';
 export const PROMETHEUS_RSOCKET_PROXY_NAME = 'prometheus-rsocket-proxy';
 export const DEFAULT_REQUIRED_DATA_VALUES = [
-  'scdf.server.image.tag=2.8.1',
+   'scdf.server.image.tag=2.8.1',
   'scdf.skipper.image.tag=2.7.1',
   'scdf.ctr.image.tag=2.8.1'
 ];
