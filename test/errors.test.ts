9ab9d9aa5fbdc19c266bd1d9d06a86c0deb8bb27
Janne Valkealahti <janne.valkealahti@gmail.com>
2021-10-19 11:04:31 +0100

    Better validation for required options

--- test/errors.test.ts
+++ test/errors.test.ts
@@ -3,9 +3,10 @@ import { execYtt } from '../src/ytt';
 describe('errors', () => {
   it('should get error with missing options', async () => {
     const result = await execYtt({ files: ['config'] });
-    // disabled fow now until we get back checks
-    // when tag or digest needs to be defined
-    // expect(result.success).toBeFalsy();
-    // expect(result.stderr).toContain('The following required data.values parameters are missing');
+    expect(result.success).toBeFalsy();
+    expect(result.stderr).toContain('Validation failed with following errors');
+    expect(result.stderr).toContain('scdf.server.image.tag');
+    expect(result.stderr).toContain('scdf.skipper.image.tag');
+    expect(result.stderr).toContain('scdf.ctr.image.tag');
   });
 });
